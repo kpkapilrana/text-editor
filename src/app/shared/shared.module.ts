e1835991c491f06e2ab77263c65169af9ba97a95
kapilpatil05 <kapil@verificient.com>
2021-05-17 14:25:10 +0530

    updated code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from "@angular/common";
 
 import { TranslateModule } from "@ngx-translate/core";
 
-import { PageNotFoundComponent } from "./components/";
 import { WebviewDirective } from "./directives/";
 import { FormsModule } from "@angular/forms";
 import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
 
 @NgModule({
-  declarations: [PageNotFoundComponent, WebviewDirective],
+  declarations: [WebviewDirective],
   imports: [CommonModule, TranslateModule, FormsModule],
   exports: [TranslateModule, WebviewDirective, FormsModule],
 })
